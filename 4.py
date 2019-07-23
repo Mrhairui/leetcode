1d83912a85d41d485e7a14f9b7ad6eae82836be7
Mrhairui <chenwentao_622@163.com>
2019-07-23 23:03:27 +0800

    fdf

--- 4.py
+++ 4.py
@@ -3,50 +3,28 @@ def median(A, B):
     n = len(B)
     if m > n:
         return median(B, A)
-    k = (m + n +1)//2
+    k = (m + n +1)//2  # 不管m+n是偶数还是奇数，这个值都是他们的和的一半
     left = 0
-    right = m
+    right = m  # 用两个指针分别指二分法查找的首尾两端
     while left < right:
-
-
-
-
-
-
-
-
-    while (len(A) > 1) or (len(B) > 1):
-        m = len(A)
-        n = len(B)
-
-
-        if m % 2 == 0:
-            meda = (A[m // 2] + A[m // 2 -1])/2
+        m1 = left + (right - left) // 2  # 二分法，也就是查找的通用伎俩，从中间比较，然后一半一半的查找
+        m2 = k - m1
+        if A[m1] < B[m2 - 1]:  # 由于下标是从零开始的，所以k-2应该是两个下标的和，所以B的right的第一个值是m-1
+            left = m1 + 1
         else:
-            meda = A[m // 2]
+            right = m1
 
-        if n % 2 == 0:
-            medb = (B[n // 2] + B[n // 2 -1])/2
-        else:
-            medb = B[n // 2]
-
-        if meda < medb:
-            A = A[(m // 2):m]
-            B = B[0:(n // 2)]
-        elif meda > medb:
-            A = A[0:(m // 2)]
-            B = B[(n // 2):n]
-        else:
-            medium = meda
-            break
-        if (len(A) <= 1) and (len(B) <= 1):
-            medium = (meda + medb) / 2
-            break
-    return medium
+    m1 = left
+    m2 = k - m1
+    c1 = max(A[m1-1] if m1 > 0 else float('-inf'), B[m2 - 1] if m2 >0 else float('-inf'))  # 一种简单的表达式的方法
+    if (m + n) % 2 == 1:
+        return c1
+    c2 = min(A[m1] if m1 < m else float('inf'), B[m2] if m2 < n else float('inf'))
+    return (c1+c2)/2
 
 
-A = [1,2,3,4,5]
-B = [6,7,8]
+A = [1,2,3,4,5,6]
+B = [5,6,7,8,9,10]
 cc = median(A, B)
 print(cc)
 

6a92c2e752f739f4ee3bc850d9f546b0b0477197
Mrhairui <chenwentao_622@163.com>
2019-08-27 22:57:11 +0800

    fdf

---
+++ 21.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists) -> ListNode:
+        import heapq
+        dummy = ListNode(0)
+        p = dummy
+        head = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(head, (lists[i].val, i))
+                lists[i] = lists[i].next
+        while head:
+            val, idx = heapq.heappop(head)
+            p.next = ListNode(val)
+            p = p.next
+        return dummy
+
+
+solution = Solution()
+l1 = ListNode(1)
+l1.next = ListNode(4)
+l1.next.next = ListNode(5)
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+l3 = ListNode(2)
+l3.next = ListNode(6)
+lists = [l1, l2, l3]
+
+a = solution.mergeKLists(lists)
+

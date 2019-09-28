63e3ebbb6bfd2e55656ff7d26b0b3e804d889f4b
Mrhairui <chenwentao_622@163.com>
2019-09-28 22:53:20 +0800

    fdf

--- 30.py
+++ 30.py
@@ -17,7 +17,7 @@ class Solution:
         n = len(res)
         while i < n:
             j = i
-            while j < n-1 and res[j+1] == res[j] + 1
+            while j < n-1 and res[j+1] == res[j] + 1:
                 j +=1
             ans = max(ans, j-i+1)
             i = j + 1

5bf985e042f8a09a84d061fde3f93df5c99fdcfb
Mrhairui <chenwentao_622@163.com>
2019-10-30 23:04:13 +0800

    fdf

--- 40.py
+++ 40.py
@@ -2,9 +2,12 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         left = 0
-        middle = 1
-        right = 2
         n = len(height)
+        while left < n:
+            if height[left] > height[left+1]:
+                break
+        right = left + 1
+
         while right < n:
             if middle < left and middle < right:
                 if left <= right:

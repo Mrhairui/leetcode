af3652538c996d001ba2d0d01209ddf6efcef03a
Mrhairui <chenwentao_622@163.com>
2019-11-05 22:42:42 +0800

    fdf

--- 40.py
+++ 40.py
@@ -3,12 +3,41 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         left = 0
         n = len(height)
+        res = 0
+        right = n-1
         while left < n:
             if height[left] > height[left+1]:
                 break
-        right = left + 1
+            left += 1
+        while right >= 0:
+            if height[right] > height[right-1]:
+                break
+            right -= 1
+        p = height[left:right + 1]
 
-        while right < n:
-            if middle < left and middle < right:
-                if left <= right:
-                    water = left - middle
+        index = height.index(max(height))
+        pp = left
+        while left < index:
+            pp = pp + 1
+            if height[pp] >= height[left]:
+                left = pp
+            res = res + (height[left] - height[pp])
+        pp = right
+        while right > index:
+            pp = pp - 1
+            if height[pp] >= height[right]:
+                right = pp
+            res = res + (height[right] - height[pp])
+
+        return res
+
+
+
+
+if __name__ == '__main__':
+    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    solution = Solution()
+    result = solution.trap(height)
+    print(result)
+
+
